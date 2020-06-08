7f6758c8c8762d1b137d4a6c50d77ebb6e35cadb
Ricardo Caldeira <ricardocaldeira@gmail.com>
2020-06-08 14:49:41 -0300

    user model

--- app/app/calc.py
+++
@@ -1,5 +0,0 @@
-
-
-def add(x, y):
-    """Adds two number"""
-    return x + y
--- app/app/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-from app.calc import add
-
-class CalcTests(TestCase):
-    def test_add_numbers(self):
-        """Test that 2 numbers are added"""
-        self.assertEqual(add(3,8), 11)
