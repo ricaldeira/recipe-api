271108d32303435ce713ad1bf810857315afb202
Ricardo Caldeira <ricardocaldeira@gmail.com>
2020-06-08 16:23:15 -0300

    custom user

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,43 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        print("[AdminSiteTest]:: Setting up")
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@haupai.com",
+            password="1234"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="test@haupai.com",
+            password="1234",
+            name="Teste User",
+        )
+
+    def test_users_listed(self):
+        """Test that user are listed on users page"""
+        print("[AdminSiteTest]:: Test User Listed")
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Teste that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        #/admin/core/user/1
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+    
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
