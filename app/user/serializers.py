1e6deb28d73574c25e387a694127fdce800c724a
Ricardo Caldeira <ricardocaldeira@gmail.com>
2020-06-09 05:19:12 -0300

    teste users

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -26,8 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
         user = super().update(instance, validated_data)
         if password:
             user.set_password(password)
-            user.save()
-
+        user.save()
         return user
 
 
