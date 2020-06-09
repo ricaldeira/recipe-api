872d7772198fdfe6cffbdb639e2fe03cf5dbd4c9
Ricardo Caldeira <ricardocaldeira@gmail.com>
2020-06-09 05:44:27 -0300

    recipe module and docker-compose adjustments

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from app.core import models
+
+def sample_user(email='teste@haupai.com', password='123456'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -41,3 +46,11 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Meet'
+        )
+        self.assertEqual(str(tag), tag.name)
